38489f0bb0e4d8e3438bf9466cf5b061b289fffb
FellypeSouza <fellypedev23@gmail.com>
2025-03-28 16:16:37 -0300

    calculadora criada, com funções de adição, subtração, divisão e multiplicação, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'main';
+  resultado = "";
+
+  Inserir(valor:string):void{
+    this.resultado += valor;
+  }
+  Limpar():void{
+    this.resultado = "";
+  }
+  BackSpace():void{
+    this.resultado = this.resultado.slice(0, -1)//pega todos os caracteres da string, menos o ultimo
+  }
+  Calcular(operacao:string):void{
+    this.resultado = eval(operacao);
+  }
+  NovosBotoes():void{
+
+  }
 }
