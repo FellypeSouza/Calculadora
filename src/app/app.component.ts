fe2c68bcb3eeedb335892b27031a76b00671c23c
FellypeSouza <fellypedev23@gmail.com>
2025-04-14 16:00:45 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,55 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'main';
-  resultado = "";
+  resultado = '0';
 
   Inserir(valor:string):void{
+    if (this.resultado == '0') {
+      this.resultado = '';  
+    }
     this.resultado += valor;
   }
   Limpar():void{
-    this.resultado = "";
+    this.resultado = "0";
   }
   BackSpace():void{
     this.resultado = this.resultado.slice(0, -1)//pega todos os caracteres da string, menos o ultimo
+    if (this.resultado == '') {
+      this.resultado = '0'
+    }
   }
   Calcular(operacao:string):void{
     this.resultado = eval(operacao);
   }
-  NovosBotoes():void{
+  CalcularPotenciacao(valor:string):void{
 
+  }
+  CalcularRaizQuadrada(operacao:string):void{
+    
+  }
+  Sen(valor:string):void{
+    
+  }
+  Cos(valor:string):void{
+
+  }
+  Tan(valor:string):void{
+
+  }
+  NovosBotoes():void{
+    let botaoEscondido = document.getElementsByClassName("botaoEscondido");
+    
+    //For para acessar todos os botoes com a classe botaoEscondido
+    for(let i = 0; i < botaoEscondido.length; i++){
+      let botao = botaoEscondido[i] as HTMLElement;
+      const estiloBotao = window.getComputedStyle(botao).display;
+
+      if (estiloBotao === "none") {
+        botao.style.display = "block";
+      }
+      else{
+        botao.style.display = "none";
+      }
+    }
   }
 }
